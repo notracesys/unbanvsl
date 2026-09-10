62e689803e2db0388d61c399656dd011902ee968
Stullet <lirabusiness7@gmail.com>
2026-09-10 03:33:11 +0000

    tá certo?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB-placeholder",
-  authDomain: "project-id.firebaseapp.com",
-  projectId: "project-id",
-  storageBucket: "project-id.firebasestorage.app",
+  apiKey: "COLE_SUA_API_KEY_AQUI",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO_ID",
+  storageBucket: "SEU_PROJETO.firebasestorage.app",
   messagingSenderId: "000000000000",
   appId: "1:000000000000:web:000000000000"
 };
