8510dc084ee68e8cc09b49a01dadba752c90a871
Stullet <lirabusiness7@gmail.com>
2026-09-10 03:34:27 +0000

    criei e agora?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
-
+// Cole aqui os dados gerados no console do seu Firebase (Engrenagem > Configurações do Projeto)
 export const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY_AQUI",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO_ID",
-  storageBucket: "SEU_PROJETO.firebasestorage.app",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:000000000000"
+  apiKey: "COLE_SUA_API_KEY_AQUI", // Substitua isso pela sua apiKey real
+  authDomain: "unban-a07e6.firebaseapp.com",
+  projectId: "unban-a07e6",
+  storageBucket: "unban-a07e6.firebasestorage.app",
+  messagingSenderId: "367123908471", // Pode manter ou atualizar com o seu real
+  appId: "1:367123908471:web:substitua_pelo_seu_appid" // Substitua pelo seu appId real
 };
